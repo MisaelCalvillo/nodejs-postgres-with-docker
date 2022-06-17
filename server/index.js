ac98d1b652ee53ec8d6bbd2ed821cf36f4e347cc
Misael Calvillo Mancilla <misa@homely.mx>
2022-06-16 20:38:55 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,17 @@ const PORT = process.env.PORT || 8000
 
 console.log(process.env.NODE_ENV);
 
+function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  throw new Error();
+  res.status(500)
+  res.render('error', { error: err })
+}
+
+app.use(errorHandler);
+
 app.get('/', (req, res) => {
   res.send('Servidor de Devf DevOps funcionando ✅')
 })
@@ -12,6 +23,8 @@ app.get('/', (req, res) => {
 app.get('/error', (req, res) => {
   throw new Error();
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`El servidor esta corriendo en http://localhost:${PORT}`)
